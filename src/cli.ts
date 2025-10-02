206218b6330128e73f67bf5b31a553cd5af22093
DuninD <dunin-04@mail.ru>
2025-10-02 23:12:07 +0500

    add logging and DI infrastructure

--- src/cli.ts
+++ src/cli.ts
@@ -1,13 +1,13 @@
 #!/usr/bin/env node
 
+import axios from 'axios';
 import chalk from 'chalk';
 import fs from 'node:fs';
-import axios from 'axios';
 import path, { dirname } from 'node:path';
 import { fileURLToPath } from 'node:url';
 import { RentalOffer } from './models/rental-offer.js';
-import { TSVWriter } from './TSVWriter.js';
-import { TSVReader } from './TSVReader.js';
+import { TSVWriter } from './cli/TSVWriter.js';
+import { TSVReader } from './cli/TSVReader.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -19,32 +19,44 @@ const showHelp = () => {
   console.log(chalk.blue('Список команд:'));
   console.log(`${chalk.green('--help:')} выводит информацию о командах`);
   console.log(`${chalk.green('--version:')} выводит версию приложения`);
-  console.log(`${chalk.green('--import <filename>:')} импортирует данные из TSV-файла`);
+  console.log(
+    `${chalk.green('--import <filename>:')} импортирует данные из TSV-файла`
+  );
 };
 
 const importData = async (filePath: string): Promise<void> => {
   const reader: TSVReader = new TSVReader(filePath);
   let readNext = true;
 
-  while (readNext){
+  while (readNext) {
     const result = await reader.getRentalOffer();
     if (!result) {
       readNext = false;
       continue;
     }
     const [rentalOffer, countRentalOffers] = result as [RentalOffer, number];
-    console.log(chalk.green(`Успешно импортированы : ${countRentalOffers} предложений аренды`), rentalOffer);
+    console.log(
+      chalk.green(
+        `Успешно импортированы : ${countRentalOffers} предложений аренды`
+      ),
+      rentalOffer
+    );
   }
 };
 
-const generateData = async (countRentalOffers: number, filePath: string, url: string): Promise<void> => {
+const generateData = async (
+  countRentalOffers: number,
+  filePath: string,
+  url: string
+): Promise<void> => {
   try {
     const response = await axios.get<RentalOffer[]>(url);
     const availableData = response.data;
 
     const tsvGenerator = new TSVWriter(filePath);
     for (let i = 0; i < countRentalOffers; i++) {
-      const randomItem = availableData[Math.floor(Math.random() * availableData.length)];
+      const randomItem =
+        availableData[Math.floor(Math.random() * availableData.length)];
       tsvGenerator.addRentalOffer(randomItem);
     }
     tsvGenerator.end();
@@ -68,8 +80,7 @@ switch (command) {
       importData(filePath);
     }
     break;
-  case '--generate':
-  {
+  case '--generate': {
     const [nArg, filePath, url] = args;
     const countRentalOffers: number = parseInt(nArg, 10);
     generateData(countRentalOffers, filePath, url);
